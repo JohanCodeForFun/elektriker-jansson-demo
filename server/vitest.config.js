b2b00019aee5e1be435ff9666fdb63c00514a82e
JohanCodeForFun <hellberg.johan@hotmail.com>
2025-09-21 11:38:55 +0200

    format document

--- server/vitest.config.js
+++ server/vitest.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    environment: 'node',
+    environment: "node",
     coverage: {
-      provider: 'v8',
-      reporter: ['text', 'lcov'],
-      reportsDirectory: 'coverage'
-    }
-  }
+      provider: "v8",
+      reporter: ["text", "lcov"],
+      reportsDirectory: "coverage",
+    },
+  },
 });
