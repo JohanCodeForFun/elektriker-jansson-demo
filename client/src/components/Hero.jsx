f4e793f60ff719a9cb060e6ee46c59234d007e72
JohanCodeForFun <hellberg.johan@hotmail.com>
2025-09-29 13:54:33 +0200

    add strong dev branch paragraph

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,13 +1,18 @@
 function Hero({ setShowModalPlaning }) {
-    function handleClick () {
-    setShowModalPlaning(prev => !prev)
+  function handleClick() {
+    setShowModalPlaning((prev) => !prev);
   }
 
-  return ( 
+  return (
     <section className="hero">
       <h1>Elektriker Jansson</h1>
       <p>Din pålitliga partner för alla elarbeten</p>
-      <button className="button button--cta" onClick={handleClick}>BÖRJA DIN PLANERING</button>
+      <p>
+        <strong>DEVELOPMENT BRANCH</strong>
+      </p>
+      <button className="button button--cta" onClick={handleClick}>
+        BÖRJA DIN PLANERING
+      </button>
     </section>
   );
 }
